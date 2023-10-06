9f7711b1fd80b3c9706497bdd6258b61efcfdba4
NajmyW <120731308+NajmyW@users.noreply.github.com>
2023-10-06 10:36:11 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+const { randomBytes } = require("crypto")
+const axios = require("axios")
+
+async function chatGpt(query){
+try {
+
+const { id_ }= (await axios.post("https://chat.chatgptdemo.net/new_chat",{user_id: "crqryjoto2h3nlzsg"},{headers:{
+"Content-Type": "application/json",
+
+}})).data
+
+const json = {"question":query,"chat_id": id_,"timestamp":new Date().getTime()}
+
+
+const { data } = await axios.post("https://chat.chatgptdemo.net/chat_api_stream",json,{headers:{
+"Content-Type": "application/json",
+
+}})
+const cek = data.split("data: ")
+
+let res = []
+
+for (let i=1; i < cek.length; i++){
+if (cek[i].trim().length > 0){
+res.push(JSON.parse(cek[i].trim()))
+}}
+
+return res.map((a) => a.choices[0].delta.content).join("")
+
+} catch (error) {
+console.error("Error parsing JSON:",error)
+return 404
+}
+}
+
+//chatGpt("what is school?")
